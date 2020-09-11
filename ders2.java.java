a70886182afdee2868f95b8540642e63870d7b4d
OĞUZHAN TÜRK <cenipon@.github.com>
2020-09-11 16:44:44 +0300

    ikinci göndemem farklı dosya

---
+++ ders2.java.java
@@ -0,0 +1,31 @@
+package ders2;
+
+
+public class ders2 {
+
+	public static void main (String[]args){
+		
+
+			
+			int f=2,m=5,e=6,c=8;
+			String kad="TUNCAY TÜRK";
+			
+			
+			System.out.println("FEN NOTU   "+f);
+			System.out.println("MATEMATÝK NOTU   "+m);
+			System.out.println("EDEBÝYAT NOTU   "+e);
+			System.out.println("COÐRAFYA NOTU   "+c);
+			System.out.println("KULLANICI ADI   "+kad);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+
+}
