97a6bb3a12d9a6320fadab06c6bc1c7b819b0b1d
OĞUZHAN TÜRK <cenipon@.github.com>
2020-09-11 16:39:55 +0300

    ilk ekleme

---
+++ src/ders3/ders3.java
@@ -0,0 +1,24 @@
+package ders3;
+
+public class ders3 {
+public static void main(String[]args)
+{
+int e=2,c=3,m=4,f=5;
+String k="TUNCAY TÜRK";
+
+System.out.println("EDEBÝYAT NOTU   "+e);
+System.out.println("COÐRAFYA NOTU   "+c);
+System.out.println("MATEMATÝK NOTU  "+m);
+System.out.println("FENBÝLGÝSÝ NOTU "+f);
+System.out.println("KULLANICI  "+k);
+
+
+
+
+
+
+
+
+}
+	
+}
